caa2b81927caac92a9e7b8088d042201ad2f0181
Noula <kouodjipb@gmail.com>
2021-12-08 18:48:21 -0500

    mise en ligne
    Modification des classes

---
+++ Models/CarteCreditModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abonnement.Models
+{
+    class CarteCredit
+    {
+        public int NumeroCarte { get; set; }
+        public DateTime DateExpiration { get; set; }
+        public int CodeSecret { get; set; }
+
+        public Client clients { get; set; }
+    }
+}
---
+++ Models/Film.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abonnement.Entite
+{
+    public class Film
+    {
+        public string Titre { get; set; }
+        public int Duree { get; set; }
+
+        public string Description { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
