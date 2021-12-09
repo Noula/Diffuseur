5bb902b87f48077c952fb34c34f8c524346c8c32
marc pipa djole <pidjoma08@outlook.fr>
2021-12-09 03:56:49 -0500

    Ajout modelesVue et nuget caliburn

---
+++ GestionAbonnesFilms/GestionAbonnesFilmsUI/VueModeles/ConnexionVueModele.cs
@@ -0,0 +1,11 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionAbonnesFilmsUI.VueModeles
+{
+    public class ConnexionVueModele :Conductor<object>
+    {
+    }
+}
